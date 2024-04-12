36725fac287846682b00871742b91daf1996c5bd
timepassuser <useless.as.shit.com@gmail.com>
2024-04-12 06:24:45 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,59 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+    async fetch(request, env, ctx) {
+        console.log(request);
+        const corsHeaders = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET,OPTIONS",
+            "Access-Control-Allow-Headers": "corsproxy,urltofetch"
+        };
+
+        async function handleOptions(request) {
+            if (
+                request.headers.get("Origin") !== null &&
+                request.headers.get("Access-Control-Request-Method") !== null &&
+                request.headers.get("Access-Control-Request-Headers") !== null
+            ) {
+                // Handle CORS preflight requests.
+                var response = new Response(null, {
+                    headers: {
+                        ...corsHeaders,
+                    },
+                });
+                return response;
+            } else {
+                // Handle standard OPTIONS request.
+                return new Response(null, {
+                    headers: {
+                        Allow: "GET, HEAD, POST, OPTIONS",
+                    },
+                });
+            }
+        }
+
+        if (request.method === "OPTIONS") {
+            // Handle CORS preflight requests
+            return handleOptions(request);
+        }
+        var headers = request.headers;
+        if (request.headers.get("corsproxy") && request.headers.get("urltofetch")) {
+            var urltofetch = request.headers.get("urltofetch");
+            if (urltofetch.includes("https://") && urltofetch.includes("cdn3.digialm.com") && urltofetch.includes(".html")) {
+                try {
+                    var response = await fetch(urltofetch, {
+                        signal: AbortSignal.timeout(2000)
+                    });
+                    response = new Response(response.body, response);
+                } catch (error) {
+                    var response = new Response(`Error ${error}`);
+                }
+            } else {
+                var response = new Response("This worker only fetches a certain type of url");
+            }
+        } else {
+            var response = new Response("Invalid Request");
+        }
+        response.headers.append("Access-Control-Allow-Origin", corsHeaders["Access-Control-Allow-Origin"]);
+        response.headers.append("Access-Control-Allow-Headers", corsHeaders["Access-Control-Allow-Headers"]);
+        return response;
+    },
 };
